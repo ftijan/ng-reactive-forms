28385d0fcc4d13b68240a43c281851d47e03a380
Filip Tijan <ftijan@gmail.com>
2022-01-05 21:17:36 +0000

    buildAddress helper method

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -57,14 +57,7 @@ export class CustomerComponent implements OnInit {
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
       sendCatalog: true,
-      addresses: this.fb.group({
-        addressType: 'home',
-        street1: '',
-        street2: '',
-        city: '',
-        state: '',
-        zip: ''
-      })      
+      addresses: this.buildAddress()    
     });
 
     this.customerForm.get('notification')?.valueChanges.subscribe(
@@ -78,6 +71,17 @@ export class CustomerComponent implements OnInit {
     .subscribe(
       value => this.setMessage(emailControl)
     );
+  }
+
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
   }
 
   populateTestData(): void {
